714595fd254c13ca7e16e4feb5faf5ed421ae4f6
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-26 15:22:12 -0600

    Added Jenkins configuration

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 #
 
-IMAGE_NAME='com.gft/zuul:0.0.1-SNAPSHOT'
+IMAGE_NAME='com.gft/oauth2:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11001:11001 $IMAGE_NAME
+CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11002:11002 $IMAGE_NAME`
+echo 'Created container with ID' $CONTAINER_ID
+
+echo 'Waiting 30s'
+sleep 30s
+
+docker logs $CONTAINER_ID
+
 echo 'Success'
