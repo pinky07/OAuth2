e49d80d5eeac79da8145c8df5ed107df631d5c0d
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-31 10:15:46 -0600

    Changed port to 11003 + Small improvements

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
 
-IMAGE_NAME='com.gft/oauth2:0.0.1-SNAPSHOT'
+IMAGE_NAME='com.gft.oauth2:latest'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11002:11002 $IMAGE_NAME`
+CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11003:11003 $IMAGE_NAME`
 echo 'Created container with ID' $CONTAINER_ID
 
 echo 'Waiting 30s'
--- jenkins-stop-dev.sh
+++ jenkins-stop-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-IMAGE_NAME='com.gft/oauth2:0.0.1-SNAPSHOT'
+IMAGE_NAME='com.gft.oauth2:latest'
 echo 'Stopping containers based on' $IMAGE_NAME
 
 echo 'Searching for previously running containers...'
